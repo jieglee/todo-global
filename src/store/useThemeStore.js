a41ac800219646110de430ddc04ea806c11e467e
jieglee <callmezie23@gmail.com>
2026-08-31 12:55:23 +0700

    anjay

---
+++ src/store/useThemeStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set, get) => ({
+    isDarkMode: false,
+    toggleTheme: () =>
+        set((state) => ({
+            isDarkMode: !state.isDarkMode,
+        })),
+}));
